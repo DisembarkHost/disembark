ec5e92dd4879d44f635164571e901e57037454df
Austin Ginder <austinginder@gmail.com>
2025-10-09 07:13:19 -0400

    🚀 RELEASE: v1.1.0

--- disembark-connector.php
+++ disembark-connector.php
@@ -9,7 +9,7 @@
  * Plugin Name:       Disembark Connector
  * Plugin URI:        https://disembark.host
  * Description:       Connector plugin for Disembark
- * Version:           1.0.7
+ * Version:           1.1.0
  * Author:            Austin Ginder
  * Author URI:        https://austinginder.com
  * License:           MIT
