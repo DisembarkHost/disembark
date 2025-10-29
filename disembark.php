1d8edc454b66921b35af8c60d7d0adba86e082fe
Austin Ginder <austinginder@gmail.com>
2025-10-28 21:24:45 -0400

    🚀 RELEASE: v2.3.0

--- disembark.php
+++ disembark.php
@@ -9,7 +9,7 @@
  * Plugin Name:       Disembark
  * Plugin URI:        https://disembark.host
  * Description:       Safe journeys as you travel to your next WordPress host.
- * Version:           2.2.1
+ * Version:           2.3.0
  * Author:            Austin Ginder
  * Author URI:        https://austinginder.com
  * License:           MIT
