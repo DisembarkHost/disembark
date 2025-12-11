c0c2c6a3c3208d4ded058fe052103698526a94a4
Austin Ginder <austinginder@gmail.com>
2025-12-11 18:38:50 -0500

    👌 IMPROVE: PHP7 polyfill

--- disembark.php
+++ disembark.php
@@ -23,6 +23,12 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
+if ( ! function_exists( 'str_starts_with' ) ) {
+    function str_starts_with( $haystack, $needle ) {
+        return (string)$needle !== '' && strncmp($haystack, $needle, strlen($needle)) === 0;
+    }
+}
+
 require plugin_dir_path( __FILE__ ) . 'vendor/autoload.php';
 
 /**
